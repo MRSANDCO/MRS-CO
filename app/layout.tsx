ea5e8812890dc65282b7278ae2b4e4c37c3bed3a
Deepma1283 <jaiswaldeepma1283@gmail.com>
2025-09-27 23:33:58 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const inter = Inter({
 })
 
 export const metadata = {
-  title: 'MRS CO Website',
-  description: '',
+  title: 'MRS & Co. Chartered Accountants',
+  description: 'MRS & Co. Chartered Accountancy firm in Ghaziabad, offers expert solutions in Income Tax Filing (ITR), GST registration & filing, company incorporation, auditing, forensic accounting and compliance services. We specialize in helping startups, small businesses, and professionals with hassle-free accounting, bookkeeping, payroll, and ROC/MCA filings. Our mission is to simplify tax and compliance, so you can focus on growing your business. With a client-first approach, transparent pricing, and timely services, we are recognized as ONE OF THE BEST CA FIRMS in Ghaziabad. Contact us for reliable tax consultancy, audit, and business registration support.',
 }
 
 export default function RootLayout({
