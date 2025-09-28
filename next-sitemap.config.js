6bf0e12a865a0aa6c7130599d0b8261247d073e8
Deepma1283 <jaiswaldeepma1283@gmail.com>
2025-09-28 16:14:35 +0530

    sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mrsandco.in', // replace with your domain
+  generateRobotsTxt: true, // also generate robots.txt
+  sitemapSize: 5000,
+};
